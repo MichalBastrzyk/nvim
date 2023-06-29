311ec12caa32c25524a72ad2c970d3698fafe77d
Michal Bastrzyk <michal.bastrzyk.pl@gmail.com>
2023-06-29 20:04:02 +0200

    feat: Added keybind to paste from system clipboard

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,4 +22,5 @@ vim.keymap.set("n", "<C-s>", "<cmd>:w<cr>", { silent = true })
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
 vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank whole line to system clipboard" })
 
--- TODO: Paste from systemclipboard *once*
+vim.keymap.set({ "n", "v" }, "<leader>P", [["+P"]], { desc = "Paste from system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p"]], { desc = "Paste from system clipboard" })
